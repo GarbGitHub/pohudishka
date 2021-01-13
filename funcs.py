4dec468f6872ad51521a52727ea25d7511a432c3
Sergei_Klyuikov <garbich@gmail.com>
2021-01-13 21:04:54 +0300

    добавлена функция отображения профиля пользователя, импортированы новые пакеты, создание графиков

--- funcs.py
+++ funcs.py
@@ -1,8 +1,13 @@
+import numpy as np
 from flask import render_template, session, abort, redirect, url_for, request, flash
 from datetime import datetime
 import model
+import modules
+from modules import menu, deleting_files, graph
 import user_authorization
 import user_registration
+import matplotlib.pyplot as plt
+import random
 
 
 def check_user_authorization():
@@ -13,12 +18,11 @@ def check_user_authorization():
     return user
 
 
-menu = [{"name": "Мой вес", "url": "/"},
-        {"name": "Добавить запись", "url": "/add/"}]
-
-
 def route_index():
-    submenu = [{"name": "Добавить", "url": "/add/"}]
+    menu = modules.menu.menu()
+    user_menu = modules.menu.user_menu(check_user_authorization())
+    submenu = modules.menu.submenu_weight()
+
     if request.method == 'POST':
         id_element = request.form['btn_id']  # del
 
@@ -30,19 +34,84 @@ def route_index():
     if ('username' in session) and ('user_id' in session):
         list = model.UserWeight.query.filter_by(user_id=session['user_id']).order_by(
             model.UserWeight.created_at.desc()).all()
+
+        y1_list = []
+        date = []
+
+        for el in reversed(list):
+            date.append(el.created_at.strftime("%d.%m.%Y"))
+            y1_list.append(el.real_weight)
+
+        x_list = (range(0, len(y1_list)))
+
+        plt.xticks(x_list, date)
+        plt.plot(x_list, y1_list, marker='.')
+
+        graph_img_name = f'static/users/{check_user_authorization().lower()}/graph/g-{random.randint(1, 5000)}.png'
+        print(graph_img_name)
+
+        plt.savefig(graph_img_name)
+        plt.show()
+
     else:
         return redirect(url_for('login'))
 
     return render_template('index.html',
                            title='Мой вес',
                            menu=menu,
+                           user_menu=user_menu,
                            submenu=submenu,
+                           graph_img_name=graph_img_name,
                            list=list,
                            session=check_user_authorization())
 
 
+def route_weight(username):
+    menu = modules.menu.menu()
+    user_menu = modules.menu.user_menu(check_user_authorization())
+    submenu = modules.menu.submenu_weight()
+
+    if request.method == 'POST':
+        id_element = request.form['btn_id']  # del
+
+        # если нажата кнопка удалить
+        if id_element.split("_")[1] == 'del':
+            model.remove_from_db(model.UserWeight, id_element.split("_")[0], session['user_id'])
+
+    # если пользователь авторизован
+    if ('username' in session) and ('user_id' in session):
+        check_user = check_user_authorization()
+        folder = f'static/users/{check_user.lower()}/graph/'
+        graph_img_name = f'static/users/{check_user.lower()}/graph/g-{random.randint(1, 5000)}.png'
+
+        # чистим пользовательскую папку с графиками
+        modules.deleting_files.delete(folder)
+
+        # запрос показателей веса пользователя из БД
+        list = model.UserWeight.query.filter_by(user_id=session['user_id']).order_by(
+            model.UserWeight.created_at.desc()).all()
+
+        # создается график
+        modules.graph.create_graph(check_user, list, graph_img_name)
+
+    else:
+        return redirect(url_for('login'))
+
+    return render_template('index.html',
+                           title='Мой вес',
+                           menu=menu,
+                           user_menu=user_menu,
+                           submenu=submenu,
+                           graph_img_name=graph_img_name,
+                           list=list,
+                           session=check_user)
+
+
 def route_add():
-    submenu = [{"name": "Мой вес", "url": "/"}]
+    menu = modules.menu.menu()
+    user_menu = modules.menu.user_menu(check_user_authorization())
+    submenu = modules.menu.submenu_add(check_user_authorization())
+
     if ('username' in session) and ('user_id' in session):
         pass
     if request.method == 'POST':
@@ -62,7 +131,7 @@ def route_add():
             model.add_object_to_base(obj)
 
             flash(f'Запись успешно добавлена', category='success')
-            return redirect(url_for('index'))
+            return redirect(url_for('weight', username=session['username']))
 
         except ValueError:
             flash(f'Ошибка ввода! Недопустимый тип введенных данных', category='danger')
@@ -72,18 +141,34 @@ def route_add():
     return render_template('add.html',
                            title='Новая запись',
                            menu=menu,
+                           user_menu=user_menu,
                            submenu=submenu,
                            session=check_user_authorization())
 
 
-def route_login_username(username):
+def route_profile():
+    # если пользователь авторизован
+    if ('username' in session) and ('user_id' in session):
+        return redirect(url_for('login_username', username=session['username']))
+
+    # иначе показываем форму авторизации
+    else:
+        return redirect(url_for('login'))
+
+
+def route_profile_username(username):
+    menu = modules.menu.menu()
+    user_menu = modules.menu.user_menu(check_user_authorization())
+    submenu = modules.menu.submenu_profile_username()
+
     # если пользователь авторизован
     if 'username' not in session or session['username'] != username:
-        abort(401)
-    submenu = [{"name": "Выйти", "url": "/logout"}]
+        return redirect(url_for('login'))
+
     return render_template('profile.html',
                            title=f'Профиль пользователя "{username}"',
                            menu=menu,
+                           user_menu=user_menu,
                            submenu=submenu,
                            username=username,
                            session=check_user_authorization())
@@ -105,7 +190,7 @@ def route_login():
             s = request.form['username']
             session['user_id'] = user_id
             flash(f'Вы успешно вошли как {s}!', category='info')
-            return redirect(url_for('index'))
+            return redirect(url_for('weight', username=session['username']))
         else:
             flash(f'Не верный логин или пароль', category='danger')
 
@@ -113,14 +198,18 @@ def route_login():
     if ('username' in session) and ('user_id' in session):
 
         # направляем в профиль
-        return redirect(url_for('login_username', username=session['username']))
+        return redirect(url_for('weight', username=session['username']))
 
     # иначе показываем форму авторизации
     else:
-        submenu = [{"name": "Регистрация", "url": "/registration/"}]
+        menu = modules.menu.menu()
+        user_menu = modules.menu.user_menu(check_user_authorization())
+        submenu = modules.menu.submenu_login()
+
         return render_template('login.html',
                                title="Авторизация",
                                menu=menu,
+                               user_menu=user_menu,
                                submenu=submenu,
                                session=check_user_authorization())
 
@@ -134,18 +223,24 @@ def rout_logout():
 
 
 def rout_registration():
-    submenu = [{"name": "Войти", "url": "/login"}]
+    menu = modules.menu.menu()
+    user_menu = modules.menu.user_menu(check_user_authorization())
+    submenu = modules.menu.submenu_registration()
     user_registration.user_registration_and_verification()
 
     return render_template('registration.html',
                            title='Регистрация',
                            menu=menu,
+                           user_menu=user_menu,
                            submenu=submenu,
                            session=check_user_authorization())
 
 
 def rout_pageNoteFound(error):
+    menu = modules.menu.menu()
+    user_menu = modules.menu.user_menu(check_user_authorization())
     return render_template('page_404.html',
                            title='Ошибка 404',
                            menu=menu,
+                           user_menu=user_menu,
                            session=check_user_authorization()), 404
