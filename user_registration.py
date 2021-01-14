f5da7d448ce860281635ffa5d6c360ca130bed4d
Sergei_Klyuikov <garbich@gmail.com>
2021-01-14 17:34:59 +0300

    Усилена валидация формы регистрации, добавлены функция  Поиск зарезервированных слов и запрещенных символов: search_bad_symbols_and_username

--- user_registration.py
+++ user_registration.py
@@ -5,6 +5,28 @@ import hashlib
 import model
 
 
+def search_bad_symbols_and_username(username):
+    """Поиск зарезервированных слов и запрещенных символов"""
+
+    search_bad_symbols_result = 'No'
+    bad_username = ['root', 'admin', 'moderator', 'support', 'supports', 'helpdesk']
+    bad_symbols = ['&', ' ', '=', '+', '<', '>', ',', '.', '\"', '\'', '?', '!', ':', ';', '/', '\\', '|', '#', '|',
+                   '^']
+
+    for el in bad_username:
+        if el == username:
+            search_bad_symbols_result = f'Ошибка! Нельзя использовать {username} - зарезервированное имя!'
+            break
+
+    if search_bad_symbols_result == 'No':
+        for el in bad_symbols:
+            if str(username).find(el) != -1:
+                search_bad_symbols_result = f'Ошибка! В Login найден запрещенный символ ({el})'
+                break
+
+    return search_bad_symbols_result
+
+
 def user_verification_on_the_server(us, em):
     """Поиск на совпадение имени пользователя и email в БД при регистрации пользователя"""
 
@@ -40,30 +62,35 @@ def user_registration_and_verification():
                     and len(request.form['password0']) > 4 \
                     and request.form['password0'] == request.form['password']:
                 print('ok')
-                search_user_result = user_verification_on_the_server(username, request.form['email'])
 
-                if search_user_result == 'uniq':  # если username и email уникальные
-                    hash = hashlib.md5(request.form['password0'].encode()).hexdigest()
-                    print(str(hash))
-
-                    res = model.Users(
-                        username=username,
-                        email=request.form['email'],
-                        password_hash=hash
-                    )
-
-                    result_registration = model.add_object_to_base(res)
-
-                    if result_registration is not None:  # если запись в БД не успешная
-                        flash('Ошибка при регистрации', 'danger')
-                    else:
-                        os.mkdir(f'static/users/{username.lower()}')
-                        os.mkdir(f'static/users/{username.lower()}/graph')
-                        os.mkdir(f'static/users/{username.lower()}/profile')
-                        flash('Вы успешно зарегистрированы', category='success')
-
+                symbols_and_username_result = search_bad_symbols_and_username(username)
+                if symbols_and_username_result != 'No':
+                    flash(symbols_and_username_result, 'danger')
                 else:
-                    flash(f'Запись ({search_user_result}) уже есть в базе', category='danger')
+                    search_user_result = user_verification_on_the_server(username, request.form['email'])
+
+                    if search_user_result == 'uniq':  # если username и email уникальные
+                        hash = hashlib.md5(request.form['password0'].encode()).hexdigest()
+                        print(str(hash))
+
+                        res = model.Users(
+                            username=username,
+                            email=request.form['email'],
+                            password_hash=hash
+                        )
+
+                        result_registration = model.add_object_to_base(res)
+
+                        if result_registration is not None:  # если запись в БД не успешная
+                            flash('Ошибка при регистрации', 'danger')
+                        else:
+                            os.mkdir(f'static/users/{username.lower()}')
+                            os.mkdir(f'static/users/{username.lower()}/graph')
+                            os.mkdir(f'static/users/{username.lower()}/profile')
+                            flash('Вы успешно зарегистрированы', category='success')
+
+                    else:
+                        flash(f'Запись ({search_user_result}) уже есть в базе', category='danger')
             else:
                 flash('Ошибка ввода данных', 'danger')
         except TypeError:
