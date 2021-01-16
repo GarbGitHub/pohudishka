7fc2b23fd6c3fb1e8d3a6a72432aa9530d2c62e0
Sergei_Klyuikov <garbich@gmail.com>
2021-01-16 23:27:37 +0300

    незначительная чистка кода

--- modules/graph.py
+++ modules/graph.py
@@ -1,24 +1,15 @@
 import matplotlib.pyplot as plt
 
 
-def create_graph(list, graph_img_name):
-    y_list = []
-    date = []
-
-    for el in reversed(list):
-        date.append(el.created_at.strftime("%d.%m.%Y"))
-        y_list.append(el.real_weight)
-
+def create_graph(graph_img_name, date, y_list):
+    date_format_date = [el.strftime("%d.%m.%Y") for el in date]
     x_list = (range(0, len(y_list)))
-
     plt.figure(figsize=(9, 5), dpi=100)
-    plt.xticks(x_list, date, rotation=90)
+    plt.xticks(x_list, date_format_date, rotation=90)
     plt.ylabel('Вес, кг')
+    plt.title(f'{date[0].strftime("%d.%m.%Y")} - {date[len(date) - 1].strftime("%d.%m.%Y")}')
     plt.plot(x_list, y_list, marker='.')
     fig = plt.gcf()
     fig.patch.set_facecolor('#f8f9fa')
     fig.subplots_adjust(bottom=0.23)
-    print(graph_img_name)
-
     plt.savefig(graph_img_name, facecolor=fig.get_facecolor(), transparent=True)
-    # plt.show()
