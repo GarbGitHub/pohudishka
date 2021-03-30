f787b19eb8e9e64479d1a28a96ee84feb9129559
Sergei_Klyuikov <garbich@gmail.com>
2021-03-30 14:17:14 +0300

    renamed the variable prelist -> pre_list

--- user_authorization.py
+++ user_authorization.py
@@ -7,17 +7,17 @@ def search_for_matches(us, psw):
     search = False
     user_id = 0
     data = model.Users.query.order_by(model.Users.username, model.Users.password_hash, model.Users.id).all()
-    prelist = []
+    pre_list = []
     for el in data:
         if el.username == us:
-            prelist.append(el.id)
-            prelist.append(el.password_hash)
+            pre_list.append(el.id)
+            pre_list.append(el.password_hash)
             search = True
             break
 
     if search:
         """Если username есть в БД, сравниваем пароли"""
-        if prelist[1] == hashlib.md5(psw.encode()).hexdigest():
-            user_id = prelist[0]
+        if pre_list[1] == hashlib.md5(psw.encode()).hexdigest():
+            user_id = pre_list[0]
 
     return user_id
